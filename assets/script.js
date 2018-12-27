72b0967a96bb7fd8e56c14388b25ecfd91ac5c46
unknown <oscar@globalrugbynetwork.com>
2018-12-27 02:27:36 -0600

    Instalacion de archivos, estructura de dash y funcionalidades mas complicadas.

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+function addClassCloseOne() {
+  var element = document.getElementById("notificationBoxOne");
+  element.classList.add("close-box");
+}
+
+
+function addClassCloseTwo() {
+  var element = document.getElementById("notificationBoxTwo");
+  element.classList.add("close-box");
+}
